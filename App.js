7784cd2ca48640e5be6c153a1ada1b33c000c282
ramkrishna88 <chowdaryrk077@gmail.com>
2023-11-20 12:21:14 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text
+        style={{
+          alignItems: 'center',
+        }}>
+        Hello World
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
